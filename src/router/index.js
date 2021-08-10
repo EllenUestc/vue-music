4fad9cf5c9cf208232d058480ae28feef44e82b6
yaoyanyan <yaoyanyan@cmiot.chinamobile.com>
2021-08-10 10:23:08 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,34 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Recommend from '@/views/recommend'
+import Singer from '@/views/singer'
+import TopList from '@/views/top-list'
+import Search from '@/views/search'
+import SingerDetail from '@/views/singer-detail'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/recommend'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer,
+    children: [{
+      path: ':id',
+      component: SingerDetail
+    }]
+  },
+  {
+    path: '/top-list',
+    component: TopList
+  },
+  {
+    path: '/search',
+    component: Search
   }
 ]
 
---
+++ src/views/top-list.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="top-list">
+    top-list
+  </div>
+</template>
